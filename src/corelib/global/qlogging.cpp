aaf6dfaa80dcc302afb5a20edca5f2705576931d
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-06-06 10:59:35 +0100

    WIP
    Change-Id: Iba3d9503fd30bd7326e62b616e3723d8d03e294d

--- src/corelib/global/qlogging.cpp
+++ src/corelib/global/qlogging.cpp
@@ -1445,7 +1445,7 @@ QString qFormatLogMessage(QtMsgType type, const QMessageLogContext &context, con
             message.append(QString::number(qt_gettid()));
         } else if (token == qthreadptrTokenC) {
             message.append(QLatin1String("0x"));
-            message.append(QString::number(qlonglong(QThread::currentThread()->currentThread()), 16));
+            message.append(QString::number((vaddr_t)(quintptr)QThread::currentThread()->currentThread(), 16));
 #ifdef QLOGGING_HAVE_BACKTRACE
         } else if (token == backtraceTokenC) {
             QMessagePattern::BacktraceParams backtraceParams = pattern->backtraceArgs.at(backtraceArgsIdx);
