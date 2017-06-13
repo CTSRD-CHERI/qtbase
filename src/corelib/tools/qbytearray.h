531a596f70dd96b193b095b2ff2aa04431427180
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-06-13 17:11:13 +0100

    Disable QByteArray::number with __(u)intcap_t
    Callers should be using quint64/qvaddr instead
    
    Change-Id: Icee501c643a4042011f8fe724a6f84155fd000eb

--- src/corelib/tools/qbytearray.h
+++ src/corelib/tools/qbytearray.h
@@ -373,6 +373,11 @@ public:
     Q_REQUIRED_RESULT static QByteArray number(qlonglong, int base = 10);
     Q_REQUIRED_RESULT static QByteArray number(qulonglong, int base = 10);
     Q_REQUIRED_RESULT static QByteArray number(double, char f = 'g', int prec = 6);
+#ifdef __CHERI__
+    // XXXAR: disable these for now
+    Q_REQUIRED_RESULT static QByteArray number(__uintcap_t, int base = 10) = delete;
+    Q_REQUIRED_RESULT static QByteArray number(__intcap_t, int base = 10) = delete;
+#endif
     Q_REQUIRED_RESULT static QByteArray fromRawData(const char *, int size);
     Q_REQUIRED_RESULT static QByteArray fromBase64(const QByteArray &base64, Base64Options options);
     Q_REQUIRED_RESULT static QByteArray fromBase64(const QByteArray &base64); // ### Qt6 merge with previous
