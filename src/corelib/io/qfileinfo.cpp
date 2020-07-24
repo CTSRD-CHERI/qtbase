d24bd95c3f13bf4fbf7152871abaff74803021fc
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-07-24 18:27:47 +0100

    Fix mismerge
    Change-Id: I51b5585e874a873c2ef95b4596f05319682e8afd

--- src/corelib/io/qfileinfo.cpp
+++ src/corelib/io/qfileinfo.cpp
@@ -709,8 +709,7 @@ bool QFileInfo::exists(const QString &file)
     QFileSystemEntry entry(file);
     QFileSystemMetaData data;
     // FIXME: still needed?
-    memset(&data, 0, sizeof(data)); // Avoid copying garbage from stack in QFileInfoPrivate::QFileInfoPrivate();
-    QAbstractFileEngine *engine =
+    // memset(&data, 0, sizeof(data)); // Avoid copying garbage from stack in QFileInfoPrivate::QFileInfoPrivate();
     std::unique_ptr<QAbstractFileEngine> engine
         {QFileSystemEngine::resolveEntryAndCreateLegacyEngine(entry, data)};
     // Expensive fallback to non-QFileSystemEngine implementation
