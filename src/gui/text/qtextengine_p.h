b6b99b68de898c7f89cdb8a21ec0e10dd641ba67
pentelbart <46610525+pentelbart@users.noreply.github.com>
2021-03-07 16:45:11 +0000

    Initialise QGlyphLayout properly
    (cherry picked from commit c860eb9586478fb76400f0dc09f538e3012a4f5c)
    
    Change-Id: I0a732bfdf021e2ba2cb723eb85f4e30cc7358096

--- src/gui/text/qtextengine_p.h
+++ src/gui/text/qtextengine_p.h
@@ -208,7 +208,15 @@ struct QGlyphLayout
 
     int numGlyphs;
 
-    inline QGlyphLayout() : numGlyphs(0) {}
+    inline QGlyphLayout()
+        : numGlyphs(0),
+          offsets(Q_NULLPTR),
+          glyphs(Q_NULLPTR),
+          advances(Q_NULLPTR),
+          justifications(Q_NULLPTR),
+          attributes(Q_NULLPTR)
+    {
+    }
 
     inline explicit QGlyphLayout(char *address, int totalGlyphs)
     {
